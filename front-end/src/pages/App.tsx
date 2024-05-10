597dc3d8535deefb1e83f3d715e6b84e5cf285eb
hwalker145 <hwalker145@gmail.com>
2024-05-10 12:06:55 -0400

    configuring tailwind

---
+++ front-end/src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hey
+      </header>
+    </div>
+  );
+}
+
+export default App;
