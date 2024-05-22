534c46aa25d2f617953310a5d3ef63628380707e
hwalker145 <hwalker145@gmail.com>
2024-05-22 15:08:44 -0400

    Built out RESTful API

--- back-end/flask-demo.py
+++ back-end/flask-demo.py
@@ -1,9 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    ...
+def root_request():
+    response = make_response(
+        "Bad request to root\n"
+    )
+    response.status_code = 404
+    return response
+
+@app.route("/sentiment/<ticker>")
+def sentiment_request(ticker):
+    response = make_response(
+        "Sentiment with " + ticker + "\n"
+    )
+    response.status_code = 200
+    return response
+
+@app.route("/action/<id>")
+def do_action(id):
+    response = make_response(
+        "Do action " + id + "\n"
+    )
+    response.status_code = 200
+    return response
+
+@app.route("/ticker/<fst>")
+def get_tickers(fst):
+    response = make_response(
+        jsonify({
+            "ticker": "AAPL"
+        })
+    )
+    response.status_code = 200
+    return response
 
 app.run()
