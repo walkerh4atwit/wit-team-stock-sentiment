c9365635987d440975ca34988b458ac46601e92e
hwalker145 <hwalker145@gmail.com>
2024-05-23 00:30:02 -0400

    Wireframe coming along

--- front-end/src/components/TopLinks.tsx
+++ front-end/src/components/TopLinks.tsx
@@ -2,14 +2,14 @@ import '../styles/App.css'
 import '../styles/TopLinks.css'
 
 let TopLinks = () => {
-  return (
-    <div className='pl-8 Top-links'>
-      <div>Henry</div>
-      <div>Walker</div>
-      <div>Is</div>
-      <div>Cool</div>
-    </div>
-  )
+	return (
+		<div className='pl-8 Top-links'>
+			<div>Henry</div>
+			<div>Walker</div>
+			<div>Is</div>
+			<div>Cool</div>
+		</div>
+	)
 }
 
 export default TopLinks
--- front-end/src/pages/App.tsx
+++ front-end/src/pages/App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import TopLinks from '../components/TopLinks'
-import "../index.css"
+import Container from 'react-bootstrap/Container'
+import { Row, Col, Form } from 'react-bootstrap'
+import CSS from 'csstype'
 import "../styles/App.css"
+import "../styles/bg-gradient.css"
 
-let App = () => {
-  return (
-    <>
-      <div className="bg-gradient-to-t from-cyan-200 to-blue-500 App-header">
-        <TopLinks/>
-      </div>
-      <div> 
-        Yo
-      </div>
-    </>
-  );
+const App = () => {
+	return (
+		<>
+			<div className="my-Header-Gradient App-header">
+				<TopLinks />
+			</div>
+			<Container bg-light fluid className='my-Form-Gradient my-Form-Container'>
+				<Form>
+					<Row>
+						<Form.Group as={Col}>
+							Hello
+						</Form.Group>
+						<Form.Group as={Col}>
+							It's
+						</Form.Group>
+						<Form.Group as={Col}>
+							Henry
+						</Form.Group>
+					</Row>
+				</Form>
+			</Container>
+		</>
+	);
 }
 
 export default App;
