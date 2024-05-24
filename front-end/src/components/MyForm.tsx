acf94006e5c0fbb5c71a631c28ec82cceb060bf6
hwalker145 <hwalker145@gmail.com>
2024-05-24 12:25:59 -0400

    Don't know what I did

--- front-end/src/components/MyForm.tsx
+++ front-end/src/components/MyForm.tsx
@@ -1,24 +1,36 @@
 import React from 'react'
-import { Row, Col, Form, Container } from 'react-bootstrap'
+import { Row, Col, Form, } from 'react-bootstrap'
 import "../styles/bg-gradient.css"
 
 const MyForm = () => {
     return (
-        <Container bg-light fluid className='my-Form-Gradient my-Form-Container'>
-            <Form>
-                <Row>
-                    <Form.Group as={Col}>
-                        Hello
-                    </Form.Group>
-                    <Form.Group as={Col}>
-                        It's
-                    </Form.Group>
-                    <Form.Group as={Col}>
-                        Henry
-                    </Form.Group>
-                </Row>
-            </Form>
-        </Container>
+        <Form bg-light className='my-Form-Gradient my-Form-Container'>
+            <Row>
+                <Form.Label column>
+                    Step 1
+                </Form.Label>
+                <Form.Label column>
+                    Step 2
+                </Form.Label>
+                <Form.Label column>
+                    Step 3
+                </Form.Label>
+            </Row>
+            <Row>
+                <Form.Group as={Col} style={{'padding': '1rem'}}>
+                    <Form.Check type="radio" label="Radio1" 
+                    name="formHorizontalRadios" style={{'paddingBottom': '1rem'}}/>
+                    <Form.Check type="radio" label="Radio2" 
+                    name="formHorizontalRadios"/>
+                </Form.Group>
+                <Form.Group as={Col}>
+                    
+                </Form.Group>
+                <Form.Group as={Col}>
+
+                </Form.Group>
+            </Row>
+        </Form>
     )
 }
 
