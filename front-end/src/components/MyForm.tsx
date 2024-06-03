c1c9a6000c91b869384f22704965a11a0f01510f
hwalker145 <hwalker145@gmail.com>
2024-06-03 15:48:29 -0400

    Changed front-end for presentation

--- front-end/src/components/MyForm.tsx
+++ front-end/src/components/MyForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Col, Form, } from 'react-bootstrap'
+import { Row, Col, Form, Button as BootButton } from 'react-bootstrap'
 import SearchBar from './SearchBar'
 import "../styles/bg-gradient.css"
 
@@ -12,24 +12,27 @@ const MyForm = () => {
         <Form className='my-Form-Gradient my-Form-Container'>
             <Row>
                 <Form.Label column>
-                    Step 1
+                    
                 </Form.Label>
                 <Form.Label column>
-                    Step 2
+                    Choose a stock
                 </Form.Label>
                 <Form.Label column>
-                    Step 3
+                    
                 </Form.Label>
             </Row>
             <Row>
                 <Form.Group as={Col} style={{'padding': '1rem'}}>
-                    <Form.Check type="radio" label="Radio1" 
+                    {/* <Form.Check type="radio" label="Radio1" 
                     name="formHorizontalRadios" style={{'paddingBottom': '1rem'}}/>
                     <Form.Check type="radio" label="Radio2" 
-                    name="formHorizontalRadios"/>
+                    name="formHorizontalRadios"/> */}
                 </Form.Group>
                 <Form.Group as={Col} style={{'padding': '1rem'}}>
                     <SearchBar optionMapper={dropDownHandler}/>
+                    <BootButton variant='success'>
+                        Submit!
+                    </BootButton>
                 </Form.Group>
                 <Form.Group as={Col}>
 
