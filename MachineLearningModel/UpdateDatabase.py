57a8ed575bbd89bda3a724a5441ad37bfb5d69ee
Evan Dyer <dyere1@wit.edu>
2024-06-06 23:41:17 -0400

    added ability to push articles, stocks, and sentiment socres to DB

--- MachineLearningModel/UpdateDatabase.py
+++ MachineLearningModel/UpdateDatabase.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import oracledb
+from datetime import datetime
 
-data = pd.read_csv("MachineLearningModel/finhub_data_sentiment.csv")
+data = pd.read_csv("MachineLearningModel/alpaca_sentimts.csv")
 
+
+#connect to db
 connection=oracledb.connect(
      config_dir=r"D:\WIT\Senior Project\Wallet_database1",
      user="admin",
@@ -13,30 +16,68 @@ connection=oracledb.connect(
 
 cursor = connection.cursor()
 
-check_query = """
+#SQL queries for db communication 
+check_ticker_query = """
 SELECT COUNT(*) FROM Tickers WHERE ticker = :1
 """
-update_query = """
-UPDATE Tickers SET sentiment_score = :1 WHERE ticker = :2
+check_article_query = """
+SELECT COUNT(*) FROM Articles WHERE title = :1
 """
-insert_query = """
-INSERT INTO Tickers (ticker, sentiment_score)
-VALUES (:1, :2)
+get_ticker_id_query = """
+SELECT id FROM Tickers WHERE ticker = :1
+"""
+get_article_id_query = """
+SELECT id FROM Articles WHERE title = :1
+"""
+insert_ticker_query = """
+INSERT INTO Tickers (ticker)
+VALUES (:1)
+"""
+insert_article_query = """
+INSERT INTO Articles (title, url, summary, date_published)
+VALUES (:1, :2, :3, TO_TIMESTAMP(:4, 'YYYY-MM-DD HH24:MI:SS'))
+"""
+insert_articleticker_query = """
+INSERT INTO ArticleTickers (article_id, ticker_id, sentiment_score)
+VALUES (:1, :2, :3)
 """
 
+#Create Tickers if not in DB else just update sentiment
 for index, row in data.iterrows():
-     ticker = row['ticker']
-     sentiment_score = row['sentiment']
+    newArticle = False
+    article = row['headline']
+    sentiment_score = row['sentiment']
+    cursor.execute(check_article_query, (article,))
+    countArticle = cursor.fetchone()[0]
+
+    if countArticle == 0:
+
+          created_at = row['created_at']
+          parsed_datetime = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ")
+          oracle_datetime = parsed_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+          cursor.execute(insert_article_query, (article, row['url'], row['summary'], oracle_datetime))
+          print(f"Inserted new article {article}")
+          newArticle = True
+
+    for ticker in str(row['symbols']).split(", "):
             
-     cursor.execute(check_query, (ticker,))
-     count = cursor.fetchone()[0]
+        cursor.execute(check_ticker_query, (ticker,))
+        countTicker = cursor.fetchone()[0]
             
-     if count > 0:
-          cursor.execute(update_query, (sentiment_score, ticker))
-          print(f"Updated sentiment score for {ticker}")
-     else:
-          cursor.execute(insert_query, (ticker, sentiment_score))
-          print(f"Inserted new ticker {ticker}")
+        if countTicker == 0:
+            cursor.execute(insert_ticker_query, (ticker,))
+            print(f"Inserted new ticker {ticker}")
+
+        if newArticle:
+            cursor.execute(get_ticker_id_query, (ticker,))
+            ticker_id = cursor.fetchone()[0]
+            cursor.execute(get_article_id_query, (article,))
+            article_id = cursor.fetchone()[0]
+            cursor.execute(insert_articleticker_query, (article_id,ticker_id,sentiment_score))
+
+
+        
         
 connection.commit()
 print("Database update complete")
