8ccfff9c72e8b80b6cf512c32461669d3cf56957
Henry Walker <walkerh4@wit.edu>
2024-07-31 00:07:16 -0400

    CSS wrestling match

--- front-end/src/pages/FAQ.tsx
+++ front-end/src/pages/FAQ.tsx
@@ -1,6 +1,8 @@
 import logo from "../images/logo-cropped.svg"
 import TopLinks, { TopLinksProps } from "../components/TopLinks"
 import resTopLinks from "../resources/topLinks.json"
+import faqs_json from "../resources/faq.json"
+import '../styles/Faq.css'
 
 const FAQ = () => {
     const topLinks: TopLinksProps = {
@@ -21,7 +23,12 @@ const FAQ = () => {
             </div>
             <div className="App-body">
                 <div className="my-Form-Gradient my-Form-Blob">
-                    Frequently Asked Questions:
+                    {faqs_json.map((faq) =>
+                        <>
+                            <div className="faq-question">{faq.question}</div>
+                            <div className="faq-answer">{faq.answer}</div>
+                        </>
+                    )}
                 </div>
             </div>
         </>
