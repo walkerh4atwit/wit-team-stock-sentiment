7f040dbe24bc4c8e529a30699856aa771e0b6cd0
hprantis <115592049+hprantis@users.noreply.github.com>
2024-08-03 12:07:24 -0400

    Update alpaca_collector.py
    Final implementation of alpaca_collector

--- data-collection/alpaca_collector.py
+++ data-collection/alpaca_collector.py
@@ -1,42 +1,26 @@
-import pandas
 from alpaca.data.requests import NewsRequest
 from alpaca.data.historical.news import NewsClient
 import pandas as pd
-from bs4 import BeautifulSoup
 import time
-import single_line_model
+import model_load_test
+import database_push
+from datetime import datetime
+from transformers import logging
+
+logging.set_verbosity_error()
 
 
 def alpaca_to_df():
 
-    news_client = NewsClient("PKIBUX4RP07Y8PQE44Q0", "9gcVRBWDtTiiOesmUV4KXnU1osSjcvzOnGkZHsRR")
-    news_request = NewsRequest(limit=50, include_content=True)
+    news_client = NewsClient("key", "secret_key")
+    news_request = NewsRequest(limit=50)
     data = news_client.get_news(news_request).dict()
 
     return pd.DataFrame(data['news'])
 
 
-def clean(df):
-    # using headline + summary instead of headline + content due to model size limitations for now
-    #for i in range(len(df['content'])):
-        #clean_content = BeautifulSoup(df["content"][i], "html.parser").get_text().replace("\n", "")
-        #df.loc[i, 'headline'] += clean_content
-    #df.drop(columns=['id', 'content', 'author', 'updated_at', 'summary', 'content', 'images', 'source'], inplace=True)
-    #df.rename(columns={"headline": "headline-content"}, inplace=True)
-
-    # headline + summary
-    for i in range(len(df['headline'])):
-        df.loc[i, 'headline-summary'] = df.loc[i, 'headline']
-        df.loc[i, 'headline'] += " "
-        df.loc[i, 'headline'] += df.loc[i, 'summary']
-
-    df.drop(columns=['id', 'content', 'author', 'updated_at', 'content', 'images', 'source'], inplace=True)
-
-    return df
-
-
-def pass_to_model(df_slice):
-    single_line_model.model_function(df_slice)
+def pass_to_model(text, symbols, url):
+    model_load_test.model_function(text, symbols, url)
 
 
 # initialize most recent timestamp
@@ -46,16 +30,16 @@ print(most_recent_timestamp)
 # run loop calling api and results on model
 while True:
 
-    dataf = clean(alpaca_to_df())
+    dataf = alpaca_to_df()
     i = 0
     while i < 50:
         if dataf.loc[i, 'created_at'] > most_recent_timestamp:
-            df_slice = dataf.loc[i]
-            print(dataf.loc[i, 'created_at'])
-            pass_to_model(df_slice)
-            # adds line to csv
-            #dataf.iloc[[i]].to_csv("temp1.csv", mode='a', header=False)
+            database_push.push_article(dataf.loc[i, "headline"], dataf.loc[i, "url"], dataf.loc[i, "summary"], dataf.loc[i, 'created_at'])
+            pass_to_model((dataf.loc[i, "headline"]), dataf.loc[i, 'symbols'], dataf.loc[i, 'url'])
+
         i += 1
 
     most_recent_timestamp = dataf.loc[0, 'created_at']
+    if datetime.now().hour == 0 and datetime.now().minute == 0:
+        database_push.delete_old_articles()
     time.sleep(30)
