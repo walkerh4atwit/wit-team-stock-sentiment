71b81d936f27c94f6f670c53d12c68a0d80769fe
Henry Walker <walkerh4@wit.edu>
2025-05-16 10:17:14 -0400

    a lot of changes in a lot of areas

--- back-end/src/wsgi.py
+++ back-end/src/wsgi.py
@@ -1,24 +1,48 @@
-from flask import Flask, make_response, jsonify, request, Response
+from flask import Flask, make_response, jsonify, request
+from flask import Response as Flask_Response
 from searchbar import getSearchOptions
 from leadertables import getLeaderTables
 from singleassetdata import getAssetData
 from article_cards import getArticleCards
-from db_connect import db_connect
-from oci_connect import oci_util
-import ipaddress, redis, sys, json
+import ipaddress, redis, sys, json, os
+from backend_util.db_connect import db_connect
+from zipfile import ZipFile
+from backend_util.oci_connect import oci_get_from_bucket
+from wsgi_util import \
+    make_backend_response_cache_hit, \
+    make_backend_response_cache_miss
 
-oci_util()
+# NOTE: Run all builds from pwd back-end
 
-# for the caching
+# Get the db credentials in the form of a wallet
+
+# Finding build env
+build_env = os.environ.get('BUILD_ENV')
+if not build_env:
+    raise KeyError('No build env specified.')
+
+# Making the name string from the build env
+db_wallet_name = 'Wallet-' + build_env + '.zip'
+
+# Streaming the wallet from oci bucket
+oci_get_from_bucket(db_wallet_name)
+
+# Now unzipping the wallet and giving it a new name, Database-Wallet
+db_wallet_file = ZipFile(db_wallet_name)
+os.remove(db_wallet_name)
+db_wallet_file.extractall('Database-Wallet')
+
+# instantiate cache client caching
 redis_client = redis.StrictRedis(host="redis-cache", port="6379", decode_responses=True)
 allowed_subnet = ipaddress.ip_network('10.0.10.0/24')
 
+# THE APP
 app = Flask(__name__)
 
 @app.errorhandler(404)
 def page_not_found(e):
     print(f"404 Error: Path '{request.path}' not found. Method: {request.method}")
-    return jsonify(error="This route does not exist on sentiments."), 404
+    return make_response("This route does not exist on sentiments back-end.", 404)
 
 # Routed to the root URL of this server
 # This route will not do anything and has
@@ -26,10 +50,9 @@ def page_not_found(e):
 @app.route("/api")
 def root_request():
     response = make_response(
-        "Bad request to root\n"
+        "Bad request to root\n", 404
     )
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.status_code = 404
     return response
 
 # This route attemps to build a sentiment
@@ -39,21 +62,20 @@ def sentiment_request(type_asset, id):
     db_conn = db_connect()
 
     response = make_response(
-        getAssetData(db_conn, type_asset, id)
+        jsonify(getAssetData(type_asset, id, db_conn)), 200
     )
+
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.status_code = 200
     return response
 
 @app.route("/api/articles/<type_asset>/<id>/<score>")
-def article_request(type_asset, id, score):
+def article_request(type_asset, id, score): 
     db_conn = db_connect()
 
     response = make_response(
-        jsonify(getArticleCards(type_asset, id, score, db_conn))
+        jsonify(getArticleCards(type_asset, id, score, db_conn)), 200
     )
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.status_code = 200
     return response
 
 # This route is when a request is made
@@ -63,10 +85,9 @@ def article_request(type_asset, id, score):
 @app.route("/api/action/<id>")
 def do_action(id):
     response = make_response(
-        "Do action " + id + "\n"
+        "Do action " + id + "\n", 200
     )
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.status_code = 200
     return response
 
 # This route helps the front-end show
@@ -85,42 +106,41 @@ def get_tickers():
         data_dict = json.loads(data_string)
 
     response = make_response(
-        jsonify(data_dict)
+        jsonify(data_dict), 200
     )
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.status_code = 200
     return response
 
 # This route provides the data
 # for the leader tables
 @app.route("/api/leadertables")
 def leaderTables():
-    data_string = redis_client.get("leadertables")
-    data_dict = {}
+    # data_string = redis_client.get("leadertables")
+    # data_dict = {}
+    if redis_client.exists('leadertables'):
+        data_string = redis_client.get('leadertables')
 
-    if data_string is None:
-        db_conn = db_connect()
-        data_dict = getLeaderTables(db_conn)
-        redis_client.setex("leadertables", 800, json.dumps(data_dict))
+        if data_string:
+            data_dict = json.loads(data_string)
+            response = make_response(jsonify(data_dict), 200)
+        else:
+            response = make_response("No cached data found for leadertables", 500)
+            
     else:
-        data_dict = json.loads(data_string)
+        response = make_backend_response_cache_miss(getLeaderTables)
 
-    response = make_response(
-        jsonify(data_dict)
-    )
     response.headers.add("Access-Control-Allow-Origin", "*")
-    response.status_code = 200
     return response
 
 @app.route("/api/cache/<data_type>", methods=['POST'])
 def cache_data(data_type):
     if data_type not in ["leadertables", "searchoptions"]:
-        return(make_response("Invalid datatype passed to API: " + data_type + "\n"))
+        return make_response("Invalid datatype passed to API: " + data_type), 500
 
     cachee: str
 
     if request.remote_addr not in allowed_subnet:
-        return(make_response("Forbidden!\n"))
+        return make_response("Forbidden!", 400)
     
     db_conn = db_connect()
 
@@ -132,6 +152,6 @@ def cache_data(data_type):
 
     redis_client.setex(data_type, 800, json.dumps(cachee))
 
-# development environment
+# local development environment
 if len(sys.argv) == 1:
     app.run(port=3131, host='0.0.0.0')
